6d181148aed2292740cf0d1cc15e0465996cb81f
Alex <bugrimov_a@bk.ru>
2021-12-04 00:05:18 +0300

    :tada: Initialize project

---
+++ src/main/java/dev/bug/spy/RedisSpyApplication.java
@@ -0,0 +1,13 @@
+package dev.bug.spy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisSpyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedisSpyApplication.class, args);
+    }
+
+}
