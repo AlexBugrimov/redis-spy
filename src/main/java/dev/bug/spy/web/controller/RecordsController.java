fa8ce26f20e377045aec3aa9628756d144e6c9d8
Alex <bugrimov_a@bk.ru>
2021-12-23 22:16:47 +0300

    🎨 Added prev and next button

--- src/main/java/dev/bug/spy/web/controller/RecordsController.java
+++ src/main/java/dev/bug/spy/web/controller/RecordsController.java
@@ -3,9 +3,7 @@ package dev.bug.spy.web.controller;
 import dev.bug.spy.model.SecurityData;
 import dev.bug.spy.service.SecurityDataService;
 import org.springframework.beans.support.PagedListHolder;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,13 +19,13 @@ public record RecordsController(SecurityDataService dataService) {
     private static final List<Integer> COUNT_CREATED_RECORDS = List.of(10, 100, 500, 1000, 5000);
 
     @GetMapping
-    public ModelAndView getAllRecords(@RequestParam(value = "page", defaultValue = "0") int page,
-                                      @RequestParam(value = "pageSize", defaultValue = "10") int pageSize) {
+    public ModelAndView getAllRecords(@RequestParam(value = "page", required = false, defaultValue = "0") int page,
+                                      @RequestParam(value = "pageSize", required = false, defaultValue = "10") int pageSize) {
         PagedListHolder<SecurityData> securityDataPage = dataService.findAll(PageRequest.of(page, pageSize));
         return new ModelAndView("records")
                 .addObject("records", securityDataPage.getPageList())
                 .addObject("counts", COUNT_CREATED_RECORDS)
-                .addObject("totalPages", new int[securityDataPage.getPageCount()])
+                .addObject("totalPages", securityDataPage.getPageCount())
                 .addObject("totalElements", securityDataPage.getNrOfElements())
                 .addObject("currentPage", page)
                 .addObject("pageSize", pageSize);
